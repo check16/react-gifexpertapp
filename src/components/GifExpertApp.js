b0c06c88f6f3d6d0cff7edfec993c9cd7f2b6de5
Antonio Sanchez <antonio.san.87@gmail.com>
2020-11-04 18:59:26 +0100

    Commit app Gifs

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr />            
+            <ol>
+                {categories.map(cat => {
+                    return <GifGrid 
+                    category= {cat}
+                    key={cat}/>
+                })
+                }
+            </ol>
+        </>
+    )
+}
